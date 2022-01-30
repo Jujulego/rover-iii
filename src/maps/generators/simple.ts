4b44f2d9cbe4e8c35e28533bf90e5f44c649810f
Julien Capellari <julien.capellari@gmail.com>
2022-01-30 12:28:16 +0100

    feat: simple generator direct inserts into db

--- src/maps/generators/simple.ts
+++ src/maps/generators/simple.ts
@@ -1,23 +1,24 @@
 import { BiomeName } from '../../biomes';
-import { ISize, Vector } from '../../math2d';
+import { db, TileEntity } from '../../db';
+import { ISize, Rect, Vector } from '../../math2d';
 
 import { Map } from '../map';
-import { Tile } from '../tile';
 
 // Generator
 export async function simpleMap(name: string, size: ISize, biome: BiomeName): Promise<Map> {
-  const tiles: Tile[] = [];
+  const tiles: TileEntity[] = [];
 
   for (let y = 0; y < size.h; ++y) {
     for (let x = 0; x < size.w; ++x) {
-      if (biome) {
-        tiles.push({
-          pos: new Vector(x, y),
-          biome
-        });
-      }
+      tiles.push({
+        map: name,
+        pos: new Vector(x, y),
+        biome
+      });
     }
   }
 
-  return await Map.fromArray(name, tiles);
+  await db.tiles.bulkPut(tiles);
+
+  return new Map(name, new Rect(0, 0, size.h - 1, size.w - 1));
 }
