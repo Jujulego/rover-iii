3bc5cc25a1e04bc3146e5c0157b6dbd51ee25ecc
Julien Capellari <julien.capellari@gmail.com>
2022-08-21 12:38:27 +0200

    feat(infra): setup logging bucket

--- infra/frontend-s3.tf
+++ infra/frontend-s3.tf
@@ -1,3 +1,4 @@
+# Code bucket
 resource "aws_s3_bucket" "frontend" {
   bucket = "jujulego-ants-frontend"
 }
@@ -5,6 +6,12 @@ resource "aws_s3_bucket" "frontend" {
 resource "aws_s3_bucket_acl" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_logging" "frontend" {
+  bucket        = aws_s3_bucket.frontend.id
+  target_bucket = aws_s3_bucket.frontend-logs.id
+  target_prefix = "log/"
 }
 
 resource "aws_s3_bucket_public_access_block" "frontend" {
@@ -49,4 +56,33 @@ data "aws_iam_policy_document" "frontend-s3-policy" {
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   policy = data.aws_iam_policy_document.frontend-s3-policy.json
+}
+
+# Logs bucket
+resource "aws_s3_bucket" "frontend-logs" {
+  bucket = "jujulego-ants-frontend-logs"
+}
+
+resource "aws_s3_bucket_acl" "frontend-logs" {
+  bucket = aws_s3_bucket.frontend-logs.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend-logs" {
+  bucket = aws_s3_bucket.frontend-logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "frontend-logs" {
+  bucket = aws_s3_bucket.frontend-logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
