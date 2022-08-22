66dc6ca2ba8c09cf2d39faa849c0daab4fd1ac15
Julien Capellari <julien.capellari@neoxia.com>
2022-08-22 19:08:39 +0200

    chore: add maths library

---
+++ libs/maths/src/rect.ts
@@ -0,0 +1,157 @@
+import { ISize, parseSizeArgs, Size, SizeArgs } from './size';
+import { VectorArgs, parseVectorArgs, IVector, Vector } from './vector';
+
+// Types
+export interface IRect {
+  t: number;
+  l: number;
+  b: number;
+  r: number;
+}
+
+export type RectArgs<O extends unknown[] = []> = [IRect, ...O] | [number, number, number, number, ...O];
+
+export type RectTLBRHolderAttr<N extends string> = `${N}${'Top' | 'Left' | 'Bottom' | 'Right'}`;
+export type RectTLWHHolderAttr<N extends string> = `${N}${'Top' | 'Left' | 'Width' | 'Height'}`;
+
+export type RectTLBRHolder<N extends string> = Record<RectTLBRHolderAttr<N>, number>;
+export type RectTLWHHolder<N extends string> = Record<RectTLWHHolderAttr<N>, number>;
+
+export type RectHolder<N extends string> = RectTLBRHolder<N> | RectTLWHHolder<N>;
+
+// Utils
+export function isRect(obj: IRect | number): obj is IRect {
+  return typeof obj === 'object';
+}
+
+export function isRectTLBRHolder<N extends string>(prefix: N, holder: RectHolder<N>): holder is RectTLBRHolder<N> {
+  return `${prefix}Bottom` in holder;
+}
+
+export function parseRectArgs<O extends unknown[]>(args: RectArgs<O>): [IRect, ...O] {
+  const [a1, a2, a3, a4, ...others] = args;
+
+  if (isRect(a1)) {
+    return args as [IRect, ...O];
+  }
+
+  if (typeof a2 === 'number' && typeof a3 === 'number' && typeof a4 === 'number') {
+    return [{ t: a1, l: a2, b: a3, r: a4 }, ...others] as [IRect, ...O];
+  }
+
+  throw new Error('Invalid arguments !');
+}
+
+// Class
+export class Rect implements IRect {
+  // Attributes
+  public t: number;
+  public l: number;
+  public b: number;
+  public r: number;
+
+  // Constructor
+  constructor(r: IRect);
+  constructor(t: number, l: number, b: number, r: number);
+  constructor(...args: RectArgs) {
+    const [{ t, l, b, r }] = parseRectArgs(args);
+    this.t = t;
+    this.l = l;
+    this.b = b;
+    this.r = r;
+  }
+
+  // Static methods
+  static fromVectors(u: IVector, v: IVector): Rect;
+  static fromVectors(xu: number, yu: number, v: IVector): Rect;
+  static fromVectors(u: IVector, xv: number, yv: number): Rect;
+  static fromVectors(xu: number, yu: number, xv: number, yv: number): Rect;
+  static fromVectors(...args: VectorArgs<VectorArgs>): Rect {
+    const [u, ...others] = parseVectorArgs(args);
+    const [v] = parseVectorArgs<[]>(others);
+
+    return new Rect(
+      Math.min(u.y, v.y),
+      Math.min(u.x, u.x),
+      Math.max(u.y, v.y),
+      Math.max(u.x, v.x),
+    );
+  }
+
+  static fromVectorSize(u: IVector, s: ISize): Rect;
+  static fromVectorSize(x: number, y: number, s: ISize): Rect;
+  static fromVectorSize(u: IVector, w: number, h: number): Rect;
+  static fromVectorSize(x: number, y: number, w: number, h: number): Rect;
+  static fromVectorSize(...args: VectorArgs<SizeArgs>): Rect {
+    const [u, ...others] = parseVectorArgs(args);
+    const [s] = parseSizeArgs<[]>(others);
+
+    return new Rect(u.y, u.x, u.y + s.h, u.x + s.w);
+  }
+
+  static fromHolder<N extends string>(prefix: N, holder: RectHolder<N>): Rect {
+    if (isRectTLBRHolder(prefix, holder)) {
+      return new Rect(
+        holder[`${prefix}Top`],
+        holder[`${prefix}Left`],
+        holder[`${prefix}Bottom`],
+        holder[`${prefix}Right`],
+      );
+    } else {
+      return Rect.fromVectorSize(
+        holder[`${prefix}Left`],
+        holder[`${prefix}Top`],
+        holder[`${prefix}Width`],
+        holder[`${prefix}Height`],
+      );
+    }
+  }
+
+  // Methods
+  within(r: IRect): boolean;
+  within(t: number, l: number, b: number, r: number): boolean;
+  within(...args: RectArgs): boolean {
+    const [r] = parseRectArgs(args);
+
+    return this.l >= r.l && this.r <= r.r && this.t >= r.t && this.b <= r.b;
+  }
+
+  contains(v: IVector): boolean;
+  contains(x: number, y: number): boolean;
+  contains(...args: VectorArgs): boolean {
+    const [v] = parseVectorArgs(args);
+    return v.x >= this.l && v.x <= this.r && v.y >= this.t && v.y <= this.b;
+  }
+
+  // Properties
+  get tl(): Vector {
+    return new Vector(this.l, this.t);
+  }
+
+  get bl(): Vector {
+    return new Vector(this.l, this.b);
+  }
+
+  get br(): Vector {
+    return new Vector(this.r, this.b);
+  }
+
+  get tr(): Vector {
+    return new Vector(this.r, this.t);
+  }
+
+  get w(): number {
+    return this.r - this.l;
+  }
+
+  get h(): number {
+    return this.b - this.t;
+  }
+
+  get size(): Size {
+    return new Size(this.w, this.h);
+  }
+}
+
+// Constants
+export const NULL_RECT = new Rect(0, 0, 0, 0);
---
+++ libs/maths/src/vector.ts
@@ -0,0 +1,165 @@
+import { IRect, parseRectArgs, RectArgs } from './rect';
+import { ISize, parseSizeArgs, SizeArgs } from './size';
+
+// Types
+export interface IVector {
+  x: number;
+  y: number;
+}
+
+export type VectorArgs<O extends unknown[] = []> = [IVector, ...O] | [number, number, ...O];
+
+export type VectorOrderMode = 'xy' | 'yx';
+export type VectorDistanceMode = 'euclidean' | 'manhattan';
+
+export type VectorHolderAttr<N extends string> = `${N}${'X' | 'Y'}`;
+export type VectorHolder<N extends string> = Record<VectorHolderAttr<N>, number>;
+
+// Utils
+export function isVector(obj: IVector | number): obj is IVector {
+  return typeof obj === 'object';
+}
+
+export function parseVectorArgs<O extends unknown[]>(args: VectorArgs<O>): [IVector, ...O] {
+  const [a1, a2, ...others] = args;
+
+  if (isVector(a1)) {
+    return args as [IVector, ...O];
+  }
+
+  if (typeof a2 === 'number') {
+    return [{ x: a1, y: a2 }, ...others] as [IVector, ...O];
+  }
+
+  throw new Error('Invalid arguments !');
+}
+
+// Class
+export class Vector implements IVector {
+  // Attributes
+  public x: number;
+  public y: number;
+
+  // Constructor
+  constructor(u: IVector);
+  constructor(x: number, y: number);
+  constructor(...args: VectorArgs) {
+    const [{ x, y }] = parseVectorArgs(args);
+    this.x = x;
+    this.y = y;
+  }
+
+  // Static methods
+  static fromSize(s: ISize): Vector;
+  static fromSize(w: number, h: number): Vector;
+  static fromSize(...args: SizeArgs): Vector {
+    const [s] = parseSizeArgs(args);
+    return new Vector(s.w, s.h);
+  }
+
+  static fromHolder<N extends string>(prefix: N, holder: VectorHolder<N>): Vector {
+    return new Vector(holder[`${prefix}X`], holder[`${prefix}Y`]);
+  }
+
+  // Methods
+  // - unary operations
+  norm(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  unit(): Vector {
+    if (this.isNull) return NULL_VECTOR;
+    return this.div(this.norm());
+  }
+
+  normal(): Vector {
+    return new Vector(this.y, -this.x);
+  }
+
+  floor(): Vector {
+    return new Vector(Math.floor(this.x), Math.floor(this.y));
+  }
+
+  // - binary operations
+  equals(v: IVector): boolean;
+  equals(x: number, y: number): boolean;
+  equals(...args: VectorArgs): boolean {
+    const [v] = parseVectorArgs(args);
+    return this.x === v.x && this.y === v.y;
+  }
+
+  compare(v: IVector): number;
+  compare(v: IVector, order: VectorOrderMode): number;
+  compare(x: number, y: number): number;
+  compare(x: number, y: number, order: VectorOrderMode): number;
+  compare(...args: VectorArgs<[VectorOrderMode?]>): number {
+    const [v, order = 'xy'] = parseVectorArgs(args);
+    const d = this.sub(v);
+
+    if (order === 'xy') {
+      return d.x === 0 ? -d.y : -d.x;
+    } else {
+      return d.y === 0 ? -d.x : -d.y;
+    }
+  }
+
+  distance(v: IVector): number;
+  distance(v: IVector, mode: VectorDistanceMode): number;
+  distance(x: number, y: number): number;
+  distance(x: number, y: number, mode: VectorDistanceMode): number;
+  distance(...args: VectorArgs<[VectorDistanceMode?]>): number {
+    const [v, mode = 'euclidean'] = parseVectorArgs(args);
+
+    switch (mode) {
+      case 'euclidean':
+        return this.sub(v).norm();
+
+      case 'manhattan':
+        return Math.abs(this.x - v.x) + Math.abs(this.y - v.y);
+    }
+  }
+
+  within(r: IRect): boolean;
+  within(t: number, l: number, b: number, r: number): boolean;
+  within(...args: RectArgs): boolean {
+    const [r] = parseRectArgs(args);
+    return this.x >= r.l && this.x <= r.r && this.y >= r.t && this.y <= r.b;
+  }
+
+  add(v: IVector): Vector;
+  add(x: number, y: number): Vector;
+  add(...args: VectorArgs): Vector {
+    const [v] = parseVectorArgs(args);
+    return new Vector(this.x + v.x, this.y + v.y);
+  }
+
+  sub(v: IVector): Vector;
+  sub(x: number, y: number): Vector;
+  sub(...args: VectorArgs): Vector {
+    const [v] = parseVectorArgs(args);
+    return new Vector(this.x - v.x, this.y - v.y);
+  }
+
+  mul(k: number): Vector {
+    return new Vector(this.x * k, this.y * k);
+  }
+
+  div(k: number): Vector {
+    return new Vector(this.x / k, this.y / k);
+  }
+
+  dot(v: IVector): number;
+  dot(x: number, y: number): number;
+  dot(...args: VectorArgs): number {
+    const [v] = parseVectorArgs(args);
+    return this.x * v.x + this.y * v.y;
+  }
+
+  // Properties
+  get isNull(): boolean {
+    return this.x === 0 && this.y === 0;
+  }
+}
+
+// Constants
+export const NULL_VECTOR = new Vector(0, 0);
