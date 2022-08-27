9b774e205e5cdcd7924bcb87159753f94644ca8d
Julien Capellari <julien.capellari@gmail.com>
2022-08-27 23:25:15 +0200

    feat(api): rework dynamodb client management

--- apps/api/src/tables/tile-maps.ts
+++ apps/api/src/tables/tile-maps.ts
@@ -1,6 +1,6 @@
 import { IRect } from '@ants/maths';
-
-import { dynamodbClient } from './client';
+import { BatchGetCommand, DynamoDBDocumentClient, GetCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 // Types
 export interface TileMap {
@@ -12,45 +12,53 @@ export interface TileMap {
 
 // Operations
 export async function listTileMaps(): Promise<TileMap[]> {
-  const client = dynamodbClient();
+  const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-  const keys = await client.query({
-    TableName: process.env.DATA_TABLE_NAME,
-    IndexName: 'table-index',
-    KeyConditionExpression: '#table = :table',
-    ExpressionAttributeNames: {
-      '#table': 'table'
-    },
-    ExpressionAttributeValues: {
-      ':table': 'tile-maps'
-    }
-  });
-
-  if (!keys.Items) {
-    return [];
-  }
-
-  const res = await client.batchGet({
-    RequestItems: {
-      [process.env.DATA_TABLE_NAME!]: {
-        Keys: keys.Items
+  try {
+    const keys = await client.send(new QueryCommand({
+      TableName: process.env.DATA_TABLE_NAME,
+      IndexName: 'table-index',
+      KeyConditionExpression: '#table = :table',
+      ExpressionAttributeNames: {
+        '#table': 'table'
+      },
+      ExpressionAttributeValues: {
+        ':table': 'tile-maps'
       }
-    }
-  });
+    }));
 
-  return res.Responses?.[process.env.DATA_TABLE_NAME!] as TileMap[] ?? [];
+    if (!keys.Items) {
+      return [];
+    }
+
+    const res = await client.send(new BatchGetCommand({
+      RequestItems: {
+        [process.env.DATA_TABLE_NAME!]: {
+          Keys: keys.Items
+        }
+      }
+    }));
+
+    return res.Responses?.[process.env.DATA_TABLE_NAME!] as TileMap[] ?? [];
+  } finally {
+    client.destroy();
+  }
 }
 
 export async function getTileMap(id: string): Promise<TileMap | undefined> {
-  const client = dynamodbClient();
+  const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-  const res = await client.get({
-    TableName: process.env.DATA_TABLE_NAME,
-    Key: {
-      id,
-      table: 'tile-maps'
-    }
-  });
+  try {
+    const res = await client.send(new GetCommand({
+      TableName: process.env.DATA_TABLE_NAME,
+      Key: {
+        id,
+        table: 'tile-maps'
+      }
+    }));
 
-  return res.Item as TileMap;
+    return res.Item as TileMap;
+  } finally {
+    client.destroy();
+  }
 }
