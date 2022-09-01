564506e8750609452b4ae06ceeb36eaa05afeae1
Julien Capellari <julien.capellari@neoxia.com>
2022-09-01 12:22:57 +0200

    fix(api): tests

--- apps/api/esbuild-plugins.js
+++
@@ -1,8 +0,0 @@
-const alias = require('esbuild-plugin-alias');
-const path = require('node:path');
-
-module.exports = [
-  alias({
-    '#node-web-compat': path.join(path.dirname(path.dirname(require.resolve('aws-jwt-verify'))), 'esm', 'node-web-compat-node.js')
-  })
-];
