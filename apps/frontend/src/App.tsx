0704a8f24be2f84d88646757765eabce683a9ae2
Julien Capellari <julien.capellari@gmail.com>
2022-09-04 11:52:20 +0200

    feat(frontend): remove auth page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,7 +5,6 @@ import Typography from '@mui/material/Typography';
 import { TopBar } from './components/bar/TopBar';
 
 // Pages
-const AuthPage = lazy(() => import(/* webpackChunkName: "auth.page" */'./pages/AuthPage'));
 const HomePage = lazy(() => import(/* webpackChunkName: "home.page" */'./pages/HomePage'));
 const EnvPage = lazy(() => import(/* webpackChunkName: "env.page" */'./pages/EnvPage'));
 
@@ -18,7 +17,6 @@ export const App: FC = () => {
       <Suspense fallback={<Typography>loading ...</Typography>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
           <Route path="/env" element={<EnvPage />} />
         </Routes>
       </Suspense>
