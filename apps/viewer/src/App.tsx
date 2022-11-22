9481bb27dd01b53b81bf845fcd0f475c1aca9cb5
Julien Capellari <julien.capellari@neoxia.com>
2022-11-22 18:19:15 +0100

    feat(world): propagate progress event

--- apps/viewer/src/App.tsx
+++ apps/viewer/src/App.tsx
@@ -1,6 +1,6 @@
 import { rect } from '@jujulego/2d-maths';
 import { RandomGenerator } from '@ants/world';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 import { worldClient } from './world-client';
 import { BiomeLayer } from './layers/BiomeLayer';
@@ -11,15 +11,24 @@ const AREA = rect({ x: 0, y: 0 }, { dx: 40, dy: 20 });
 // Utils
 const generator = new RandomGenerator(worldClient);
 
+generator.subscribe('progress', (event) => console.log(event));
+
 // Component
 export const App: FC = () => {
   // State
-  const [n, setN] = useState(0);
+  const [a, setA] = useState(0);
+  const b = useRef(0);
 
   // Effects
   useEffect(() => void (async () => {
+    if (a === 0) {
+      return;
+    }
+
+    console.group(`Generation n°${a}`);
+    console.time('random');
     await generator.run('test', {
-      chunkSize: AREA.size.dx - 1,
+      chunkSize: AREA.size.dx,
       bbox: AREA,
       biomes: {
         water: 0.3,
@@ -27,11 +36,15 @@ export const App: FC = () => {
         sand: 0.3
       }
     });
-  })(), [n]);
+    console.timeEnd('random');
+
+    b.current = a;
+    console.groupEnd();
+  })(), [a]);
 
   // Render
   return (
-    <div onClick={() => setN((old) => old+1)}>
+    <div onClick={() => setA(b.current + 1)}>
       <BiomeLayer world="test" area={AREA} />
     </div>
   );
--- libs/world/src/generators/tile-generator.ts
+++ libs/world/src/generators/tile-generator.ts
@@ -1,4 +1,5 @@
-import { IRect } from '@jujulego/2d-maths';
+import { IRect, rect } from '@jujulego/2d-maths';
+import { EventSource } from '@jujulego/event-tree';
 
 import { ITile } from '../tile';
 import { WorldClient } from '../world-client';
@@ -9,32 +10,52 @@ export interface TileGeneratorOpts {
   readonly chunkSize?: number;
 }
 
+export interface ProgressEvent {
+  readonly count: number;
+  readonly progress: number;
+}
+
+export type TileGeneratorEventMap = {
+  progress: ProgressEvent;
+};
+
 // Class
-export abstract class TileGenerator<O extends TileGeneratorOpts> {
+export abstract class TileGenerator<O extends TileGeneratorOpts> extends EventSource<TileGeneratorEventMap> {
   // Constructor
   constructor(
     protected readonly client: WorldClient,
-  ) {}
+  ) {
+    super();
+  }
 
   // Methods
   protected abstract generate(world: string, opts: O): AsyncGenerator<ITile> | Generator<ITile>;
 
   async run(world: string, opts: O): Promise<void> {
     const { chunkSize = 500 } = opts;
+    const size = rect(opts.bbox).size;
 
     let chunk: ITile[] = [];
+    let count = 0;
 
     for await (const tile of this.generate(world, opts)) {
       chunk.push(tile);
 
-      if (chunk.length > chunkSize) {
+      if (chunk.length >= chunkSize) {
         await this.client.bulkPutTile(world, chunk);
+
+        count += chunk.length;
+        this.emit('progress', { count, progress: count / (size.dx * size.dy) });
+
         chunk = [];
       }
     }
 
     if (chunk.length > 0) {
       await this.client.bulkPutTile(world, chunk);
+
+      count += chunk.length;
+      this.emit('progress', { count, progress: count / (size.dx * size.dy) });
     }
   }
 }
